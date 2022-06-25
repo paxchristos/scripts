1a455d0260bac37ca2463940aae29539a938086e
paxchristos <nielson.peter@gmail.com>
2022-06-24 21:14:47 -0500

    huzzah!

--- Windows/m4b_converter.ps1
+++ Windows/m4b_converter.ps1
@@ -2,17 +2,28 @@ $gathering   = "C:\temp"
 $directories = Get-ChildItem -Recurse -Directory 
 ForEach ($directory in $directories)
 {
-    Write-Host $directory
-    $results    = gci  -recurse |where-object {".mp3" -contains $_.extension}
+    $rtfm = "$($directory.FullName)"    
+    #Write-Host $rtfm
+    
+    cd "$rtfm"
+    $results    = Get-ChildItem -recurse -depth 1 | Where-Object { ".mp3" -contains $_.extension}
+    ##DEBUG STATEMENT##Get-ChildItem -recurse
     $count      = $results.count
     Write-Host $count
     if ($count -gt 0)
     {
-        $filename   = (dir).directory.name[0]
-        $newname    = $filename+".m4b"
-        $filename   = $filename+".mp3" 
-        cmd /c copy /b *.mp3 $filename
-        ffmpeg  -i "$filename" -c:a aac -vn "$newname"
+        Write-Host "Made it into if loop"
+        $folder     = $directory.name
+        $filename   = $directory.name+".mp3"
+        $newname    = $directory.name+".m4b"
+        
+        ##DEBUG STATEMENTS##
+        Write-Host $folder
+        Write-Host $filename
+        Write-Host $newname
+        Write-Host $PWD
+        cmd /c copy /b *.mp3 "$($filename)"
+        ffmpeg  -i "$($filename)" -c:a aac -vn "$($newname)"
         copy-item $newname $gathering
     }
 }
