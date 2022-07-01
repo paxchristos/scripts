fd953e31bb3f23aa9643460a34bd376b94479532
paxchristos <nielson.peter@gmail.com>
2022-06-30 21:31:11 -0500

    changing how scripts work

--- Windows/dual_audio.ps1
+++ Windows/dual_audio.ps1
@@ -7,10 +7,10 @@ foreach ($file in $mkv)
 {
     echo "$file"
     $tempname = "s01e$($counter).mkv"
-    $wordsathome=[System.IO.Path]::GetFileNameWithoutExtension($file)
-    mkvmerge.exe --output "$tempname" "$file" "$($counter).mka"
+    $wordsathome = [System.IO.Path]::GetFileNameWithoutExtension($file)
+    $mkaname = $wordsathome+".mka"
+    mkvmerge.exe --output "$tempname" "$file" "$mkaname"
     remove-item "$($counter).mka"
     remove-item "$file"
     move-item "$tempname" "$($wordsathome).mkv"
-    $counter++
 }
--- Windows/remove-hidub.ps1
+++ Windows/remove-hidub.ps1
@@ -3,9 +3,10 @@ $mkv = Get-ChildItem *.mkv
 foreach ($file in $mkv)
 {
     echo "$file"
+    $filename = $file.name
     $wordsathome=[System.IO.Path]::GetFileNameWithoutExtension("$file")
     mkvmerge --output split-"$wordsathome".mkv --split timestamps:00:00:10.010,01:00:00 "$file"
-    #rm split-"$wordsathome"-001.mkv
-    #rm "$file"
-    #mv "split-"$wordsathome"-002.mkv" "$file"
+    rm split-"$wordsathome"-001.mkv
+    rm "$file"
+    mv "split-"$wordsathome"-002.mkv" "$filename"
 }
