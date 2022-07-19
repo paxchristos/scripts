609940d66c8a098f096eb5ecb9dfabf3f15080c5
paxchristos <nielson.peter@gmail.com>
2022-07-19 07:57:35 -0500

    more scripting

--- Windows/check_subtitles.ps1
+++ Windows/check_subtitles.ps1
@@ -1,4 +1,7 @@
-##Returns codec for video, audio, and subtitle. You need to use a full path to a text file for mediainfo to work
+###Returns codec for video, audio, and subtitle. You need to use a full path to a text file for mediainfo to work
+
+##Used to clear out brackets
+##(Get-ChildItem -File -Recurse) | Rename-Item -NewName { $_.Name -replace "[\[\]]" }
 
 $txtpath = "C:\Users\peter\Documents\scripts\mediainfo\check_subtitles.txt"
 
