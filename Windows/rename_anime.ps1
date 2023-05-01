38ac8b46a751b5831726c5ac43ef7950ffbdd9a8
paxchrsitos <nielson.peter@gmail.com>
2023-04-30 19:00:20 -0500

    new script

---
+++ Windows/rename_anime.ps1
@@ -0,0 +1,28 @@
+$files = Get-ChildItem *.* | Where-Object { ! $_.PSIsContainer }
+$translations = Import-Csv -Path "C:\programs\scripts\translations.csv"
+
+ForEach ($file in $files)
+{
+    $filePath = $file.FullName
+    $fileName = $file.Name
+    $newFileName = $fileName -replace '\[.*?\]|\(.*?\)', ''
+    $newFileName = $newFileName -replace '\s{2,}', ''
+    $regex = 'S(\d+) -\s(\d+)'
+    if ($newFileName -match $regex)
+    {
+        $seasonNumber = [int]$Matches[1]
+        $episodeNumber = [int]$Matches[2]
+        $episodeNumberString = "{0:D2}" -f $episodeNumber
+        $seasonNumberString = "{0:D2}" -f $seasonNumber
+        $newFileName = $newFileName -replace $regex, "- S${seasonNumberString}E${episodeNumberString}"
+    }
+
+    foreach ($translation in $translations)
+    {
+        if ($newFileName.Contains($translation.OldName))
+        {
+            $newFileName = $newFileName -replace [regex]::Escape($translation.OldName), $translation.NewName
+        }
+    }
+    Rename-Item -Path $filePath -NewName $newFileName
+}
